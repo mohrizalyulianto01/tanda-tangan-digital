9765d4b5265cb22a7dadc4dea516c461f9754df0
jadikoding <mohrizalyulianto@gmail.com>
2023-02-12 21:21:12 +0700

    Ttd Digital finish

---
+++ app/src/main/java/com/jadikoding/tanda_tangan_digital/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jadikoding.tanda_tangan_digital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
